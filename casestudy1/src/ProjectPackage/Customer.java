6f92ad26f931df41712805618ff075d8f29a55a9
Preethi Varsha <mpreethivarsha@gmail.com>
2021-09-26 16:34:34 +0530

    -

---
+++ casestudy1/src/ProjectPackage/Customer.java
@@ -0,0 +1,33 @@
+package ProjectPackage;
+import java.util.*;
+class Customer extends Payment {
+    Scanner scan = new Scanner(System.in);
+    private String CustomerName ="";
+    private int VehicleType=3; //1- handicapped, 2-motor, 3- compact, 4- large
+    private float Balance = 0.0f;
+    private int fuelType=2; // 1- electric , 2- diesel
+    private int floor_no = 0;
+    private float bill = 0;
+    private String entryTime="";
+    private String exitTime="";
+    private boolean parkStatus = false;
+    Customer() {
+        setCustomername();
+        setBalance();
+        setVehicleType();
+        setFuelType();
+    }
+    void setCustomername(){
+
+    }
+    void setVehicleType(){
+
+    }
+    void setFuelType(){
+
+    }
+    void setBalance(){
+
+    }
+
+}
