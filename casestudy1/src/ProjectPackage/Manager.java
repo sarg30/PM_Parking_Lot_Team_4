f08c97c036becc312b46eb95fba0051b074c1ed2
Pranav Sutar <pranavsutar666@gmail.com>
2021-09-27 16:45:01 +0530

    Charge 3 input commit

--- casestudy1/src/ProjectPackage/Manager.java
+++ casestudy1/src/ProjectPackage/Manager.java
@@ -6,9 +6,9 @@ public class Manager {
     int sbike,struck,sscoter,sbus,svan,shandicapped,sElectricCar,sElectricBike; // These will store number of slots per floor 
     // of that particular type.
     
-    int charge1a, charge1b, charge1c; // for Motorcycles and Handicapped
-    int charge2a, charge2b, charge2c; // for Compact Vehicles
-    int charge3a, charge3b, charge3c; // for Large Vehicles
+    double charge1a, charge1b, charge1c; // for Motorcycles and Handicapped
+    double charge2a, charge2b, charge2c; // for Compact Vehicles
+    double charge3a, charge3b, charge3c; // for Large Vehicles
     // a==> for first hour
     // b==> for second hour
     // c==> for further hours
@@ -41,27 +41,27 @@ public class Manager {
     }
 
     public void GetCharges() {
-        System.out.println("Please enter the Charges for Parking Mortorcycles and Handicapped");
+        System.out.println("Please enter the Charge for Parking Mortorcycles and Handicapped");
         charge1a = sc.nextInt();
-        charge1b = sc.nextInt();
-        charge1c = sc.nextInt();
+        charge1b = charge1a/2;
+        charge1c = charge1b/2;
 
-        System.out.println("Please enter the Charges for Compact Vehicles");
+        System.out.println("Please enter the Charge for Compact Vehicles");
         charge2a = sc.nextInt();
-        charge2b = sc.nextInt();
-        charge2c = sc.nextInt();
+        charge2b = charge2a/2;
+        charge2c = charge2b/2;
 
-        System.out.println("Please enter the Charges for Parking Mortorcycles and Handicapped");
+        System.out.println("Please enter the Charge for Parking Mortorcycles and Handicapped");
         charge3a = sc.nextInt();
-        charge3b = sc.nextInt();
-        charge3c = sc.nextInt();
+        charge3b = charge3a/2;
+        charge3c = charge3b/2;
     }
 
 }
-/*Motorcycle (Bike, Scooter)
+/*
+Motorcycle (Bike, Scooter)
 Compact - (Car,Van, etc)
 Large (Trucks, Buses)
 Handicapped 
 Electric(facility to charge vehicles and pay fees, car or bike)
-The user provides charging time, and the corresponding price will be displayed on the screen
-*/
+The user provides charging time, and the corresponding price will be displayed on the screen*/
